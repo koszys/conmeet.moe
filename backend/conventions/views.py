7e05e52195c85111576701f2a7238c42fe94a4c6
koszys <forzadriftergod@gmail.com>
2026-09-16 00:23:11 -0400

    setting up

--- backend/conventions/views.py
+++ backend/conventions/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from .models import Convention
+from .serializers import ConventionDetailSerializer, ConventionListSerializer
+
+
+class ConventionListView(ListAPIView):
+    queryset = Convention.objects.filter(is_active=True)
+    serializer_class = ConventionListSerializer
+    ordering = ("starts_at",)
+
+
+class ConventionDetailView(RetrieveAPIView):
+    queryset = Convention.objects.filter(is_active=True)
+    serializer_class = ConventionDetailSerializer
+    lookup_field = "slug"
